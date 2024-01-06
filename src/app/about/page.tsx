178cfb26157a6c1f050f0c75f4e0977df706a057
Irine Stephanie <pinxhoes@gmail.com>
2024-01-06 13:39:24 +0100

    Adding Link Component Navigation

---
+++ src/app/about/page.tsx
@@ -0,0 +1,6 @@
+export const metadata = {
+    title: "About Stoopid Penguin",
+};
+export default function About() {
+    return <h1>About Penguin</h1>;
+}
---
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,13 @@
+/**
+* you can call this NotFound() or call the one from the root app
+*/
+
+export default function NotFound() {
+    return (
+        <div>
+            <h2>Review Not Found</h2>
+            <p>The coding Queen could not find requested resource</p>
+        </div>
+    );
+}
+
